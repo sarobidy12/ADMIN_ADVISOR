c05cf5f2cc5a34297c2a6936c8be6ec0924b5566
bidy <sarobidyalliance@outlook.com>
2022-01-09 00:55:35 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { hot } from "react-hot-loader";
-import { FC, useEffect, useRef} from "react";
+import { FC, useEffect, useRef,useState} from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { defaultTheme } from "./theme";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -13,8 +13,43 @@ import MomentUtils from "@date-io/moment";
 import { ImageViewerProvider } from "./components/Common/ImageViewer";
 import { getMessaging, getToken } from "firebase/messaging";
 import firebase from "firebase/compat/app";
+import {onMessageListener} from "./Firebase/Firebase";
+
 
 const App: FC = () => {
+
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({ title: "", body: "" });
+
+  console.log(show, notification);
+
+  onMessageListener()
+    .then((payload:any) => {
+      
+      setShow(true);
+
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+          .register("./firebase-messaging-sw.js")
+          .then(function (registration) {
+            if (Notification.permission == "granted") {
+              navigator.serviceWorker.getRegistration().then(function (reg: any) {
+                reg.showNotification(payload.notification.title, {
+                  body: payload.notification.body,
+                  icon: "https://admin-advisor.voirlemenu.fr/static/media/logo.8da5d5e8.png",
+                });
+              });
+            }
+          })
+          .catch(function (err) {
+            console.log("Service worker registration failed, error:", err);
+          });
+      }
+
+      console.log(payload);
+
+    })
+    .catch((err) => console.log("failed: ", err));
 
   const messaging = getMessaging();
 
